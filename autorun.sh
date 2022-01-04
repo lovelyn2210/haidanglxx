463df36f935ba70bdebb3bd4b7ee8e36ce939af8
haidanglx <40859600+haidanglx@users.noreply.github.com>
2022-01-04 23:27:09 +0800

    Create autorun.sh

---
+++ autorun.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+tmux new-session -d -s 11 'while (true) ; do az vm start --resource-group vps1 --name MyNC11; sleep 10;done'
+
+tmux new-session -d -s 12 'while (true) ; do az vm start --resource-group vps1 --name MyNC12; sleep 10;done'
+
+tmux new-session -d -s 13 'while (true) ; do az vm start --resource-group vps1 --name MyNC13; sleep 10;done'
+
+tmux new-session -d -s 14 'while (true) ; do az vm start --resource-group vps1 --name MyNC14; sleep 10;done'
+
+tmux new-session -d -s 15 'while (true) ; do az vm start --resource-group vps1 --name MyNC15; sleep 10;done'
+
+tmux new-session -d -s 16 'while (true) ; do az vm start --resource-group vps1 --name MyNC16; sleep 10;done'
+
+tmux new-session -d -s 17 'while (true) ; do az vm start --resource-group vps1 --name MyNC17; sleep 10;done'
+
+tmux new-session -d -s 18 'while (true) ; do az vm start --resource-group vps1 --name MyNC18; sleep 10;done'
+
+tmux new-session -d -s 19 'while (true) ; do az vm start --resource-group vps1 --name MyNC19; sleep 10;done'
+
+tmux new-session -d -s 20 'while (true) ; do az vm start --resource-group vps1 --name MyNC20; sleep 10;done'
+
+tmux new-session -d -s 21 'while (true) ; do az vm start --resource-group vps1 --name MyNC21; sleep 10;done'
+
+tmux new-session -d -s 22 'while (true) ; do az vm start --resource-group vps1 --name MyNC22; sleep 10;done'
+
+tmux new-session -d -s 23 'while (true) ; do az vm start --resource-group vps1 --name MyNC23; sleep 10;done'
+
+tmux new-session -d -s 24 'while (true) ; do az vm start --resource-group vps1 --name MyNC24; sleep 10;done'
+
+tmux new-session -d -s 25 'while (true) ; do az vm start --resource-group vps1 --name MyNC25; sleep 10;done'
+
+tmux new-session -d -s 26 'while (true) ; do az vm start --resource-group vps1 --name MyNC26; sleep 10;done'
+
+
